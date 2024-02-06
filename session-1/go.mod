98e4a932296a30aeab7c324a62cc875d1006a0f0
Syauqi <syauqifrizman@gmail.com>
2024-02-06 11:59:30 +0700

    feat: add variable content

---
+++ session-1/go.mod
@@ -0,0 +1,3 @@
+module session-1
+
+go 1.21.6
---
+++ session-1/main.go
@@ -0,0 +1,43 @@
+package main
+
+func main() {
+	/*
+		// with data type
+		var title string = "GO Programming Language"
+		var totalMember int = 18
+
+		var title1 string
+		var totalMember1 int
+		title1 = "GO Programming Language"
+		totalMember1 = 18
+
+		fmt.Println("Course title is: ", title)
+		fmt.Println("The total member is: ", totalMember)
+		fmt.Println("Course title is: ", title1)
+		fmt.Println("The total member is: ", totalMember1)
+	*/
+
+	/*
+		// without data type
+		title := "GO Programming Language" // short declaration technique
+		totalMember := 18
+
+		fmt.Printf("%T, %T\n", title, totalMember) // untuk print jenis tipe datanya
+		fmt.Println("Course title is: ", title)
+		fmt.Println("The total member is: ", totalMember)
+	*/
+
+	/*
+		// multiple declaration variable
+		var firstName, surName, lastName string = "Muhammad", "Syauqi", "Frizman"
+
+		var age1, age2, age3 int
+		age1, age2, age3 = 20, 21, 22
+		// age1, age2, age3 := 20, 21, 22
+
+		fmt.Println(firstName, surName, lastName)
+		fmt.Println(age1, age2, age3)
+
+		fmt.Printf("Hello, my name is %s and my age is %d years old\n", firstName, age2)
+	*/
+}
